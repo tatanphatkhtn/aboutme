4dbe0194987eff09593a0d3c172cb8520dc9eb80
1453031 <1453031@student.hcmus.edu.vn>
2017-09-21 09:05:38 +0700

    Evil listen

--- app.js
+++ app.js
@@ -21,9 +21,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(function (req, res, next) {
+  console.log('>>>>>>>>>>>>>>>>', req.path);
+  next();
+});
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
